8e63dba96379b7791ba67297e8d4d20c7d8018dd
Jon Moss <me@jonathanmoss.me>
2015-05-08 21:12:56 +0000

    Added name to users

---
+++ db/migrate/20150508211036_add_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :name, :string
+  end
+  
+  def self.down
+    remove_column :users, :name, :string
+  end
+end
