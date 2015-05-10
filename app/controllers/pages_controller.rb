1c0f390ea768413c69b5369ca725e7918b6fe47f
Jon Moss <me@jonathanmoss.me>
2015-05-09 20:32:03 -0400

    Added bootstrap styling to Devise views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
 
   def home
+    @user = current_student if student_signed_in?
+    @user = current_teacher if teacher_signed_in?
   end
-  
+
   def sign_up_selection
   end
 end
