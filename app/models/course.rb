20320fb27158bad7a0535e765466d10962c8fe00
Jon Moss <me@jonathanmoss.me>
2015-05-10 10:30:25 -0400

    Autogenerate course codes

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,19 @@
 class Course < ActiveRecord::Base
+  before_validation :generate_course_code
+
   belongs_to :teacher
 
-  validates :name, presence: true
+  validates_presence_of :name, :course_code
+  validates_uniqueness_of :course_code
+
+  private
+
+    def generate_course_code
+      code = SecureRandom.hex(3)
+      if Course.find_by(course_code: code) != nil
+        generate_course_code
+      else
+        self.course_code ||= code
+      end
+    end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
+  let(:course) { FactoryGirl.create(:course) }
+
+  it { should belong_to(:teacher) }
 
   it "has a valid factory" do
     FactoryGirl.build(:course).should be_valid
   end
 
-  it { should belong_to(:teacher) }
+  context "with no teacher" do
+    before { course.teacher_id = nil }
+    it { is_expected.not_to be_valid }
+  end
+
+  context "with no name" do
+    before { course.name = "" }
+    it { is_expected.not_to be_valid }
+  end
+
+  context "with no course code" do
+    before { course.course_code = "" }
+    it { is_expected.not_to be_valid }
+  end
 
 end
