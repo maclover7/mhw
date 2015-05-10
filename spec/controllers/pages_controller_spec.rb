e5a8044525e19b96fbddc358263ae08ccb3add7e
Jon Moss <me@jonathanmoss.me>
2015-05-10 09:23:50 -0400

    Fix tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
 
-  describe "GET #sign_up_selection" do
-    it "returns 200" do
-      get :sign_up_selection
+  describe "GET #auth" do
+    it "renders the auth template" do
+      get :auth
+      response.should render_template 'auth'
       response.status.should eq(200)
     end
   end
