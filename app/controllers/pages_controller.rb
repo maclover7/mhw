da2fcc5e38242ffca169e3cb5803894c2b952e4a
Jon Moss <me@jonathanmoss.me>
2015-05-11 16:46:58 -0400

    Working on add course modal

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,12 @@ class PagesController < ApplicationController
 
   def dashboard_student
     @student = current_student
+    @courses = current_student.courses
   end
 
   def dashboard_teacher
     @teacher = current_teacher
-    @courses = Course.where(teacher_id: current_teacher).all #current_teacher.courses
+    @courses = current_teacher.courses #Course.where(teacher_id: current_teacher).all
   end
 
   def home
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -19,7 +19,13 @@ RSpec.describe PagesController, type: :controller do
   end
 
   describe "GET #dashboard_student" do
+    let(:student) { FactoryGirl.create(:student) }
+    before { sign_in student }
+
     it "renders the dashboard_student template" do
+      course = FactoryGirl.create(:course)
+      enrollment = FactoryGirl.create(:enrollment, course_id: course, student_id: student)
+
       get :dashboard_student
       response.should render_template 'dashboard_student'
       response.status.should eq(200)
@@ -27,7 +33,12 @@ RSpec.describe PagesController, type: :controller do
   end
 
   describe "GET #dashboard_teacher" do
+    let(:teacher) { FactoryGirl.create(:teacher) }
+    before { sign_in teacher }
+
     it "renders the dashboard_teacher template" do
+      course = FactoryGirl.create(:course, teacher_id: teacher)
+
       get :dashboard_teacher
       response.should render_template 'dashboard_teacher'
       response.status.should eq(200)
