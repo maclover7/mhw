56f7db6b1fe945f3f0182fbf745d1d8ddcef23ab
maclover7 <me@jonathanmoss.me>
2015-05-26 12:45:35 -0400

    Cleaned up some cruft

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,6 @@ class StudentsController < ApplicationController
     if params[:time] == "overdue"
     elsif params[:time] == "today"
       @page_heading = "Today's Assignments:"
-      @today = Date.today.strftime("%A, %B %d")
       @student_assignments = StudentAssignment.joins(:assignment).where("DATE(assignments.due_date) = ?", Date.today).all
     elsif params[:time] == "tomorrow"
     elsif params[:time] == "week"
@@ -23,7 +22,7 @@ class StudentsController < ApplicationController
     if params[:course_id]
       if @course = current_student.courses.find_by_id(params[:course_id])
         @page_heading = "#{@course.name} Assignments:"
-        @student_assignments = StudentAssignment.where(student_id: current_student.id).all #, assignment_course_id: @course.id).all
+        @student_assignments = StudentAssignment.joins(:assignment).where("assignments.course_id = ?", @course.id).all
       else
         redirect_to student_root_path, notice: "Not authorized to view assignments for this course" if @course.nil?
       end
