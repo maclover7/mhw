64591bc94a845198094108b5708fcd6c7d7389ad
maclover7 <me@jonathanmoss.me>
2015-05-26 12:46:27 -0400

    Removed unneeded virtual attributes

--- app/models/student_assignment.rb
+++ app/models/student_assignment.rb
@@ -2,14 +2,4 @@ class StudentAssignment < ActiveRecord::Base
   belongs_to :assignment
   belongs_to :student
 
-  def assignment_course_id
-    #self.assignment.course.id
-    assignment.course.id
-  end
-
-  def assignment_due_date
-    #self.assignment.due_date
-    assignment.due_date.strftime("%A, %B %d")
-  end
-
 end
