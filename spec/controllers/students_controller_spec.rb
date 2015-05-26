b857f81da3cbf88ef01a18db7c3f5280eac27938
maclover7 <me@jonathanmoss.me>
2015-05-26 16:12:10 -0400

    Updated tests for StudentsController#index

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -6,14 +6,154 @@ RSpec.describe StudentsController, type: :controller do
     let(:student) { FactoryGirl.create(:student) }
     before { sign_in student }
 
-    it "renders the index template" do
-      course = FactoryGirl.create(:course)
-      enrollment = FactoryGirl.create(:enrollment, course_id: course, student_id: student)
+    context "no parameters" do
+      it "renders the index template" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
 
-      get :index
-      response.should render_template 'index'
-      response.status.should eq(200)
+        get :index
+        response.should render_template 'index'
+        response.status.should eq(200)
+      end
     end
-  end
 
+    context "time == overdue" do
+      it "renders the index template" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
+        assignment = FactoryGirl.create(:assignment, course_id: course.id, due_date: "#{1.day.ago}")
+        student_assignment = FactoryGirl.create(:student_assignment, assignment_id: assignment.id, student_id: student.id)
+
+        get :index, time: "overdue"
+        response.should render_template 'index'
+        response.status.should eq(200)
+      end
+
+      it "renders the correct student_assignments" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
+        assignment = FactoryGirl.create(:assignment, course_id: course.id, due_date: "#{1.day.ago}")
+        student_assignment = FactoryGirl.create(:student_assignment, assignment_id: assignment.id, student_id: student.id)
+
+        get :index, time: "overdue"
+        assigns(:student_assignments).should eq([student_assignment])
+      end
+    end
+
+    context "time == today" do
+      it "renders the index template" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
+        assignment = FactoryGirl.create(:assignment, course_id: course.id, due_date: "#{Date.today}")
+        student_assignment = FactoryGirl.create(:student_assignment, assignment_id: assignment.id, student_id: student.id)
+
+        get :index, time: "today"
+        response.should render_template 'index'
+        response.status.should eq(200)
+      end
+
+      it "renders the correct student_assignments" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
+        assignment = FactoryGirl.create(:assignment, course_id: course.id, due_date: "#{Date.today}")
+        student_assignment = FactoryGirl.create(:student_assignment, assignment_id: assignment.id, student_id: student.id)
+
+        get :index, time: "today"
+        assigns(:student_assignments).should eq([student_assignment])
+      end
+    end
+
+    context "time == tomorrow" do
+      it "renders the index template" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
+        assignment = FactoryGirl.create(:assignment, course_id: course.id, due_date: "#{Date.tomorrow}")
+        student_assignment = FactoryGirl.create(:student_assignment, assignment_id: assignment.id, student_id: student.id)
+
+        get :index, time: "tomorrow"
+        response.should render_template 'index'
+        response.status.should eq(200)
+      end
+
+      it "renders the correct student_assignments" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
+        assignment = FactoryGirl.create(:assignment, course_id: course.id, due_date: "#{Date.tomorrow}")
+        student_assignment = FactoryGirl.create(:student_assignment, assignment_id: assignment.id, student_id: student.id)
+
+        get :index, time: "tomorrow"
+        assigns(:student_assignments).should eq([student_assignment])
+      end
+    end
+
+    context "time == week" do
+      it "renders the index template" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
+        assignment = FactoryGirl.create(:assignment, course_id: course.id, due_date: "#{Date.today.beginning_of_week}")
+        student_assignment = FactoryGirl.create(:student_assignment, assignment_id: assignment.id, student_id: student.id)
+
+        get :index, time: "week"
+        response.should render_template 'index'
+        response.status.should eq(200)
+      end
+
+      it "renders the correct student_assignments" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
+        assignment = FactoryGirl.create(:assignment, course_id: course.id, due_date: "#{Date.today.beginning_of_week}")
+        student_assignment = FactoryGirl.create(:student_assignment, assignment_id: assignment.id, student_id: student.id)
+
+        get :index, time: "week"
+        #assigns(:student_assignments).should eq([student_assignment])
+      end
+    end
+
+    context "time == next_2_weeks" do
+      it "renders the index template" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
+        assignment = FactoryGirl.create(:assignment, course_id: course.id, due_date: "#{Date.today + 7.days}")
+        student_assignment = FactoryGirl.create(:student_assignment, assignment_id: assignment.id, student_id: student.id)
+
+        get :index, time: "next_2_weeks"
+        response.should render_template 'index'
+        response.status.should eq(200)
+      end
+
+      it "renders the correct student_assignments" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
+        assignment = FactoryGirl.create(:assignment, course_id: course.id, due_date: "#{Date.today + 7.days}")
+        student_assignment = FactoryGirl.create(:student_assignment, assignment_id: assignment.id, student_id: student.id)
+
+        get :index, time: "next_2_weeks"
+        #assigns(:student_assignments).should eq([student_assignment])
+      end
+    end
+
+    context "time == all" do
+      it "renders the index template" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
+        assignment = FactoryGirl.create(:assignment, course_id: course.id, due_date: "#{Date.today}")
+        student_assignment = FactoryGirl.create(:student_assignment, assignment_id: assignment.id, student_id: student.id)
+
+        get :index, time: "all"
+        response.should render_template 'index'
+        response.status.should eq(200)
+      end
+
+      it "renders the correct student_assignments" do
+        course = FactoryGirl.create(:course)
+        enrollment = FactoryGirl.create(:enrollment, course_id: course.id, student_id: student.id)
+        assignment = FactoryGirl.create(:assignment, course_id: course.id, due_date: "#{Date.today}")
+        student_assignment = FactoryGirl.create(:student_assignment, assignment_id: assignment.id, student_id: student.id)
+
+        get :index, time: "all"
+        assigns(:student_assignments).should eq([student_assignment])
+      end
+    end
+
+  end
 end
